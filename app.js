13c54fc0a227d824e2dbdd67733339ba42a29237
LucasFifa24 <lucasfifa421@gmail.com>
2026-01-10 10:40:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -22,10 +22,23 @@ function loadFavorites() {
 }
 
 function loadSettings() {
-  view.innerHTML = `
-    <h2>Settings</h2>
-    <p>Account system coming next.</p>
-  `;
+  const user = getCurrentUser();
+
+  if (user) {
+    view.innerHTML = `
+      <h2>Settings</h2>
+      <p>Logged in as <strong>${user}</strong></p>
+      <button onclick="signOut()">Sign Out</button>
+    `;
+  } else {
+    view.innerHTML = `
+      <h2>Settings</h2>
+      <input id="username" placeholder="Username">
+      <input id="password" type="password" placeholder="Password">
+      <button onclick="signIn(username.value, password.value)">Sign In</button>
+      <button onclick="signUp(username.value, password.value)">Sign Up</button>
+    `;
+  }
 }
 
 loadHome();
